591fe160f77f7c4eec8eda26a6fec5ca281399d0
shafielegacy <burn.kajang@gmail.com>
2026-06-27 19:37:08 +0800

    Refine mobile dashboard layout

--- sw.js
+++ sw.js
@@ -1,12 +1,25 @@
-const CACHE_NAME = 'upkk-v1';
+const CACHE_NAME = 'upkk-v2';
 const urlsToCache = ['./', './index.html'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, copy));
+      return r;
+    }).catch(() => caches.match(e.request))
   );
 });
